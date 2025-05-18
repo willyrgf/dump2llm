d9abd959b48a218ae4906e21f21c296788eeafc3
Willy Romão <willyr.goncalves@gmail.com>
2025-05-18 18:46:27 +0100

    add dump2llm and nix packaging

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dump2llm
+
+## Run (with Nix)
+```
+nix run github:willyrgf/dump2llm -- <git-repo-url|local-path>
+```
---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "dump2llm: dumps an git-repo or path into text for LLM chats";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages = {
+          default = self.packages.${system}.dump2llm;
+
+          dump2llm = pkgs.stdenv.mkDerivation rec {
+            pname = "dump2llm";
+            version = "0.0.1";
+            src = ./.;
+
+            buildInputs = with pkgs; [ bash git ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              substitute ${pname} $out/bin/${pname} \
+                --replace "/usr/bin/env bash" "${pkgs.bash}/bin/bash"
+              chmod +x $out/bin/${pname}
+            '';
+shellHook = '' export PATH=${pkgs.bash}/bin:$PATH
+            '';
+
+            meta = with pkgs.lib; {
+              description = "dumps an git-repo or path into text for LLM chats";
+              homepage = "https://github.com/willyrgf/dump2llm";
+              license = licenses.mit;
+              platforms = platforms.all;
+            };
+          };
+        };
+
+        apps = {
+          default = self.apps.${system}.dump2llm;
+
+          dump2llm = {
+            type = "app";
+            program = "${self.packages.${system}.dump2llm}/bin/dump2llm";
+            meta = self.packages.${system}.dump2llm.meta;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ bash git ];
+        };
+      }
+    );
+}
